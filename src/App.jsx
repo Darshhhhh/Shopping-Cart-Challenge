5af1427d40fa11ea32e724ff926ed6bb22486e88
Darshhhhh <contact.shahdarsh@gmail.com>
2023-10-13 12:09:32 -0400

    Update with fn components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./components/Home";
+import NewHome from "./components/NewHome";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <NewHome />
     </>
   );
 }
